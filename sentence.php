bd50f0a463ff7b8be4f965ac0793cce816c9dc81
Dragomir <d.galinov.d.salle@gmail.com>
2020-01-23 19:30:49 +0100

    Table creation and history created

--- sentence.php
+++ sentence.php
@@ -7,10 +7,30 @@ class SENTENCES {
         $sentenciaLetra = substr($sentencia, 0 , 1);
         if ($sentenciaLetra === "i" || $sentenciaLetra === "u" || $sentenciaLetra === "d" || $sentenciaLetra === "I" || $sentenciaLetra === "U" || $sentenciaLetra === "D") {
             $db->getConnection()->select_db($database);
+            $sql = "CREATE TABLE SqlHistory (
+                id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                sentence VARCHAR(255) NOT NULL,
+                executed_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                favourite TINYINT(255) DEFAULT 0
+                )";
+            $db->getConnection()->query($sql);
+            $insert = "INSERT INTO sqlhistory(sentence) VALUES ('$sentencia')";
+            var_dump($insert);
+            $db->getConnection()->query($insert);
             $db->getConnection()->query($sentencia);
             echo "SQL Sentence [" . $sentencia . "] successfully injected";
         } else if ($sentenciaLetra === "s" || $sentenciaLetra === "S") {
             $db->getConnection()->select_db($database);
+            $sql = "CREATE TABLE SqlHistory (
+                id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                sentence VARCHAR(255) NOT NULL,
+                executed_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                favourite TINYINT(255) DEFAULT 0
+                )";
+            $db->getConnection()->query($sql);
+            $insert = "INSERT INTO sqlhistory(sentence) VALUES ('$sentencia')";
+            var_dump($insert);
+            $db->getConnection()->query($insert);
             $db->getConnection()->query($sentencia);
             $result = $db->getConnection()->query($sentencia);
             if ($result->num_rows > 0) {
